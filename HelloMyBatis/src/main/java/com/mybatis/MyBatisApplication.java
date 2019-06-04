b25a1576186a81566d2be30a7b0d7feae0a7e27a
XIACYBING <359474377@qq.com>
2019-06-04 17:15:47 +0800

    忽略META-INF文件夹

---
+++ HelloMyBatis/src/main/java/com/mybatis/MyBatisApplication.java
@@ -0,0 +1,10 @@
+package com.mybatis;
+
+/**
+ * @program: ideaWorkspace
+ * @description: HelloMyBatis启动类
+ * @author: wang.yubin
+ * @create: 2019/06/04
+ */
+public class MyBatisApplication {
+}
---
+++ HelloMyBatis/src/main/java/com/mybatis/test/ShowPool.java
@@ -0,0 +1,10 @@
+package com.mybatis.test;
+
+/**
+ * @program: ideaWorkspace
+ * @description: 测试Tomcat数据库连接池
+ * @author: wang.yubin
+ * @create: 2019/06/04
+ */
+public class ShowPool {
+}
