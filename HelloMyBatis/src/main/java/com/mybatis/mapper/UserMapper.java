98feea32524ab344c42e4e577d3b00eef3b086fa
XIACYBING <359474377@qq.com>
2019-06-05 11:28:44 +0800

    完成增删改操作

--- HelloMyBatis/src/main/java/com/mybatis/mapper/UserMapper.java
+++ HelloMyBatis/src/main/java/com/mybatis/mapper/UserMapper.java
@@ -15,15 +15,62 @@ import java.util.Map;
  */
 public interface UserMapper {
 
+    /**
+     * 查询所有用户信息
+     * @return 返回用户信息的List集合
+     */
     List<User> selAll();
 
+    /**
+     * 根据用户id查询用户信息
+     * @param uid 用户id
+     * @return 返回一个User
+     */
     User selOne(int uid);
 
+    /**
+     * 根据用户id查询用户信息，
+     * @param uid0 用户id
+     * @param uid1 用户id
+     * @return 返回一个Map集合，key为uid，value为User对象
+     */
     @MapKey("uid")
     Map<Long,User> selMapInterface(int uid0,int uid1);
 
+    /**
+     * 使用Map查询分页
+     * @param paramMap 包含pageStart和pageSize
+     * @return 返回一个List集合
+     */
     List<User> selPageByMap(Map<String,Object> paramMap);
 
+    /**
+     * 使用@Param注解查询分页（@Param注解底层还是使用Map）
+     * @param pageStart 当前页开始条数
+     * @param pageSize 当前页数据量
+     * @return 返回一个List集合
+     */
     List<User> selPageByParam(@Param("pageStart") int pageStart,@Param("pageSize") int pageSize);
+
+    /**
+     * 插入一条用户信息
+     * @param user 包含用户信息的User对象
+     * @return 返回一个int，代表数据库中被影响的行数
+     */
+    int insOne(User user);
+
+    /**
+     * 修改zhangqi的密码
+     * @param pwd 新密码
+     * @return 返回数据库中被影响的行数
+     */
+    int updPwd(String pwd);
+
+    /**
+     * 根据用户id删除用户信息
+     * @param uid 用户id
+     * @return 返回数据库中被影响的行数
+     */
+    int delOne(Long uid);
 
 }
--- HelloMyBatis/src/main/java/com/mybatis/test/ShowUser.java
+++ HelloMyBatis/src/main/java/com/mybatis/test/ShowUser.java
@@ -10,10 +10,7 @@ import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @program: ideaWorkspace
@@ -23,11 +20,13 @@ import java.util.Map;
  */
 public class ShowUser {
 
+    @SuppressWarnings("unused")
     public static void main(String[] args) throws IOException {
         Logger logger = Logger.getLogger(ShowUser.class);
         InputStream in = Resources.getResourceAsStream("MyBatis.xml");
         SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(in);
-        SqlSession session = sessionFactory.openSession();
+        /**事务不进行自动提交*/
+        SqlSession session = sessionFactory.openSession(false);
 
         /**直接调用Mapper.xml进行selectList查询*/
         List<User> list0 = session.selectList("com.mybatis.mapper.UserMapper.selAll");
@@ -54,6 +53,7 @@ public class ShowUser {
 
         /**使用Map传递数据查询分页*/
         Map<String,Object> paramMap = new HashMap<>(2);
+        Map<String,Object> pageMap = new HashMap<>(8);
         List<User> userList;
         int pageStart,pageSize;
         pageSize = 2;
@@ -62,15 +62,49 @@ public class ShowUser {
             pageStart = i*pageSize;
             paramMap.put("pageStart",pageStart);
             userList = userMapper.selPageByMap(paramMap);
-            /*logger.info(userList.toString());*/
+            pageMap.put("第" + i + "页",userList);
         }
 
         /**使用@param注解传递数据查询分页*/
         for (int i = 0; i < 5; i++) {
             pageStart = i*pageSize;
             userList = userMapper.selPageByParam(pageStart,pageSize);
-            logger.info(userList.toString());
+            pageMap.put("第" + i + "页",userList);
         }
+
+        /**新增用户*/
+        User newUser = new User();
+        newUser.setName("zhangqi");
+        newUser.setPwd("z");
+        int i = userMapper.insOne(newUser);
+        if (i == 1){
+            logger.info("插入用户成功：" + newUser.toString());
+            session.rollback();
+        }else {
+            logger.error("插入用户失败：" + i);
+            session.rollback();
+        }
+
+        /**修改用户密码*/
+        i = userMapper.updPwd("zh");
+        if (i == 1){
+            logger.info("更新用户信息成功：" + i);
+            session.rollback();
+        }else {
+            logger.error("更新用户信息失败：" + i);
+            session.rollback();
+        }
+
+        /**删除用户*/
+        i = userMapper.delOne(12L);
+        if (i == 1){
+            logger.info("删除用户成功：" + i);
+            session.rollback();
+        }else {
+            logger.error("删除用户失败：" + i);
+            session.rollback();
+        }
+
         session.close();
     }
 
