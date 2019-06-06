138e629c00ddd6474a223513b2cc74696b49de36
XIACYBING <359474377@qq.com>
2019-06-06 16:10:48 +0800

    添加webx.ml，暂时不使用SpringBoot

--- HelloMyBatis/src/main/java/com/mybatis/MyBatisApplication.java
+++ HelloMyBatis/src/main/java/com/mybatis/MyBatisApplication.java
@@ -1,10 +1,20 @@
 package com.mybatis;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @program: ideaWorkspace
  * @description: HelloMyBatis启动类
  * @author: wang.yubin
  * @create: 2019/06/04
  */
+@SpringBootApplication
 public class MyBatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisApplication.class);
+        System.out.println("MyBatisApplication...");
+    }
+
 }
--- HelloMyBatis/src/main/java/com/mybatis/servlet/ShowPoolServlet.java
+++ HelloMyBatis/src/main/java/com/mybatis/servlet/ShowPoolServlet.java
@@ -1,10 +1,21 @@
 package com.mybatis.servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
 /**
  * @program: ideaWorkspace
  * @description: 测试Tomcat数据库连接池
  * @author: wang.yubin
  * @create: 2019/06/04
  */
-public class ShowPoolServlet {
+public class ShowPoolServlet extends HttpServlet {
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("doService");
+    }
 }
