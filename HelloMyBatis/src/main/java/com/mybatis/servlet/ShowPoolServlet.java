fd5a35bd120e522c090a7ab85af409dfa95d2fc3
XIACYBING <359474377@qq.com>
2019-07-06 01:02:48 +0800

    MyBatis的一些小修改，已经忘记是什么了

--- HelloMyBatis/src/main/java/com/mybatis/servlet/ShowPoolServlet.java
+++ HelloMyBatis/src/main/java/com/mybatis/servlet/ShowPoolServlet.java
@@ -1,10 +1,17 @@
 package com.mybatis.servlet;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * @program: ideaWorkspace
@@ -17,5 +24,25 @@ public class ShowPoolServlet extends HttpServlet {
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         System.out.println("doService");
+
+        Context context;
+        DataSource dataSource;
+        Connection connection;
+        PreparedStatement ps;
+        ResultSet rs;
+        try {
+            context = new InitialContext();
+            dataSource = (DataSource) context.lookup("java:comp/env/tomcatPool");
+            connection = dataSource.getConnection();
+            String sql = "select * from proticable";
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                System.out.println(rs.getLong(0));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
--- HelloMyBatis/src/main/java/com/mybatis/test/ShowPool.java
+++ HelloMyBatis/src/main/java/com/mybatis/test/ShowPool.java
@@ -1,5 +1,14 @@
 package com.mybatis.test;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * @program: ideaWorkspace
  * @description: 测试Tomcat数据库连接池
@@ -7,4 +16,17 @@ package com.mybatis.test;
  * @create: 2019/06/04
  */
 public class ShowPool {
+
+    public static void main(String[] args) throws NamingException, SQLException {
+        Context context = new InitialContext();
+        DataSource dataSource = (DataSource) context.lookup("java:comp/env/tomcatPool");
+        Connection connection = dataSource.getConnection();
+        String sql = "select * from proticable";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()){
+            System.out.println(rs.getLong(0));
+        }
+    }
+
 }
